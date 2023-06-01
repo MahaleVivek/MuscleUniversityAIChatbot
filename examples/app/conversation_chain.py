e856c64d13d290e336e52543906a47247eb19101
Vivek Mahale <70280845+MahaleVivek@users.noreply.github.com>
2023-06-01 12:12:49 +0530

    Update conversation_chain.py

--- examples/app/conversation_chain.py
+++ examples/app/conversation_chain.py
@@ -1,7 +1,5 @@
 from functools import lru_cache
 from typing import Callable
-
-
 from dotenv import load_dotenv
 from fastapi import Depends, FastAPI, Request, WebSocket
 from fastapi.templating import Jinja2Templates
@@ -10,13 +8,9 @@ from langchain.chat_models import ChatOpenAI
 from pydantic import BaseModel
 import uvicorn
 
-
 from lanarky.responses import StreamingResponse
 from lanarky.testing import mount_gradio_app
 from lanarky.websockets import WebsocketConnection
-
-
-
 
 load_dotenv()
 
@@ -25,31 +19,23 @@ app = mount_gradio_app(FastAPI(title="ConversationChainDemo"))
 templates = Jinja2Templates(directory="templates")
 
 
-
-
 class QueryRequest(BaseModel):
    query: str
-
-
 
 
 def conversation_chain_dependency() -> Callable[[], ConversationChain]:
    @lru_cache(maxsize=1)
    def dependency() -> ConversationChain:
 
-
        template = """You are a muscle university fitness chatbot having a conversation with a fitness enthusiast who is trying improve his fitness journey.
        you can answer the question related to fitness/ workout plans/ diet/ grocery for healthy diet. add bullet points. you can answer the user queries
        in Hinglish and use jeet selal's style to respond. restrcit the answers to fitness/ workout plans/ diet/ grocery for healthy diet. if you don't know the answer,
        don't try to make up the answer, add detailed answers and add bullet points if needed.
 
-
        {history}
-
 
        Human: {input}
        Chatbot:"""
-
 
        PROMPT = PromptTemplate(
            input_variables=["input", "history"],
@@ -64,16 +50,10 @@ def conversation_chain_dependency() -> Callable[[], ConversationChain]:
            verbose=True,
            prompt=PROMPT,
        )
-
-
    return dependency
 
 
-
-
 conversation_chain = conversation_chain_dependency()
-
-
 
 
 @app.post("/chat")
@@ -86,13 +66,9 @@ async def chat(
    )
 
 
-
-
 @app.get("/")
 async def get(request: Request):
    return templates.TemplateResponse("index.html", {"request": request})
-
-
 
 
 @app.websocket("/ws")
@@ -101,8 +77,6 @@ async def websocket_endpoint(
 ):
    connection = WebsocketConnection.from_chain(chain=chain, websocket=websocket)
    await connection.connect()
-
-
 
 
 if __name__ == "__main__":
